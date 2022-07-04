453351744b0ff92892b854d57f49d98251e430ae
Antoine Fabri <antoine.fabri@gmail.com>
2022-07-04 17:34:49 +0200

    Handle both versions of base environments

--- R/other.R
+++ R/other.R
@@ -1,6 +1,16 @@
 #' @export
 construct_idiomatic.environment <- function(x, ...) {
   # this is not very robust but might help in some useful special cases
+
+  # The name of `asNamespace("pkg")` is always "pkg" and print as `<environment: namespace:pkg>`
+  # The name of `as.environment("package:pkg")` is ALMOST always "package:pkg" and prints as
+  #  `<environment: package:pkg>` + attributes
+  # The exception is `as.environment("package:base")` which prints as
+  #   `<environment: base>` and whose name is "base"
+  # This means `asNamespace("base")` (a.k.a. `.BaseNamespaceEnv`) and
+  #   `as.environment("package:base")` (a.k.a. `baseenv()`) have the same name
+  #   but are different. So we implement a workaround.
+  if(identical(x, baseenv())) return('baseenv()')
   name <- environmentName(x)
   if (name == "R_GlobalEnv") return(".GlobalEnv")
   if (name == "base") return(".BaseNamespaceEnv")
