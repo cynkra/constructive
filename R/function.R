132df36e22ef958624da4400b069a43d9e8fcbd8
Antoine Fabri <antoine.fabri@gmail.com>
2022-07-06 04:29:21 +0200

    Implement max_body arg for construct_idiomatic.function and pass dots correctly

--- R/function.R
+++ R/function.R
@@ -1,17 +1,25 @@
 #' @export
-construct_idiomatic.function <- function(x, pipe, ...) {
+construct_idiomatic.function <- function(x, pipe, max_body = NULL, ...) {
   x_lst <- as.list(x)
+
+  if (!is.null(max_body)) {
+    body_lng <- x_lst[[length(x_lst)]]
+    if(length(body_lng) > max_body + 1) {
+      x_lst[[length(x_lst)]] <- as.call(c(head(as.list(body_lng), max_body + 1), quote(...)))
+    }
+  }
   # rlang::expr_deparse changes the body by putting parentheses around f <- (function(){})
   # so we must use regular deparse
   fun_lst <- lapply(x_lst, deparse)
   args0 <- head(fun_lst, -1)
   body0 <- fun_lst[[length(fun_lst)]]
+
   # a srcref is created if the body starts with `{`
   srcrefed <- startsWith(body0[[1]], "{")
-  args <- construct_apply(args0, "alist", language = TRUE, ...)
-  body <- construct_apply(list(body0), "quote", language = TRUE, ...)
-  env <- construct_raw(environment(x))
-  code <- construct_apply(list(args, body, env), "rlang::new_function", language = TRUE)
+  args <- construct_apply(args0, "alist", language = TRUE, pipe = pipe, max_body = max_body, ...)
+  body <- construct_apply(list(body0), "quote", language = TRUE, pipe = pipe, max_body = max_body, ...)
+  env <- construct_raw(environment(x), pipe = pipe, max_body = max_body, env_as_list = FALSE, ...)
+  code <- construct_apply(list(args, body, env), "rlang::new_function", language = TRUE, ...)
   if (srcrefed) pipe(code, "rlang::zap_srcref()", pipe) else code
 }
 
