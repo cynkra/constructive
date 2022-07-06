91eab3a3b48a085bb2f03df7b6ad55a286143745
Antoine Fabri <antoine.fabri@gmail.com>
2022-07-06 05:00:14 +0200

    Update imports

--- R/imports.R
+++ R/imports.R
@@ -1,3 +1,4 @@
-#' @importFrom rlang abort try_fetch set_names names2 caller_env %||%
-#' @importFrom utils capture.output installed.packages
+#' @importFrom rlang abort warn try_fetch set_names names2 caller_env %||%
+#' @importFrom utils capture.output installed.packages head tail
+#' @importFrom stats setNames
 NULL
