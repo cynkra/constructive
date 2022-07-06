7a21046436bdb8565e28dd88c122c463c1b1c5c2
Antoine Fabri <antoine.fabri@gmail.com>
2022-07-06 04:24:41 +0200

    Implement matrix methods

---
+++ R/matrix.R
@@ -0,0 +1,23 @@
+#' @export
+construct_idiomatic.matrix <- function(x, pipe, ...) {
+  dim <- attr(x, "dim")
+  dimnames <- attr(x, "dimnames")
+  dim_names_lst <- if (!is.null(dimnames)) list(dimnames = dimnames)
+  attr(x, "dim") <- NULL # for some reasons this remove dimnames too ???
+  attr(x, "dimnames") <- NULL
+  construct_apply(
+    c(list(x, nrow = dim[[1]], ncol = dim[[2]]), dim_names_lst),
+    "matrix",
+    new_line = TRUE,
+    pipe = pipe,
+    ...)
+}
+
+#' @export
+repair_attributes.matrix <- function(x, code, pipe ="base", ...) {
+  repair_attributes_impl(
+    x, code, pipe,
+    ignore = c("dim", "dimnames"),
+    ...
+  )
+}
