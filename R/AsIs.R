57239fc1bcdc7472f9e3f4458ebee848798daf61
Antoine Fabri <antoine.fabri@gmail.com>
2022-07-06 04:15:57 +0200

    Implement AsIs methods

---
+++ R/AsIs.R
@@ -0,0 +1,11 @@
+#' @export
+construct_idiomatic.AsIs <- function(x, ...) {
+  class(x) <- setdiff(oldClass(x), "AsIs")
+  wrap(construct_raw(x, ...), "I", new_line = FALSE)
+}
+
+#' @export
+repair_attributes.AsIs <- function(x, code, pipe ="base", ...) {
+  # no reparation needed, this will be dealt with in `I()`'s arg
+  code
+}
