003e8e5556a9dd1829bd1bb8f78407533fe7cd21
Antoine Fabri <antoine.fabri@gmail.com>
2022-07-06 04:19:11 +0200

    implement read.table arg for data frames

--- R/data.frame.R
+++ R/data.frame.R
@@ -1,13 +1,27 @@
 #' @export
-construct_idiomatic.data.frame <- function(x, ...) {
+construct_idiomatic.data.frame <- function(x, read.table = FALSE, ...) {
   df_has_list_cols <- any(sapply(x, is.list))
   # FIXME: not safe re attributes
   if(df_has_list_cols) {
-    tibble_code <- construct_apply(x, fun = "tibble::tibble", keep_trailing_comma = FALSE, ...)
+    tibble_code <- construct_apply(x, fun = "tibble::tibble", keep_trailing_comma = FALSE, read.table = read.table, ...)
     df_code <- wrap(tibble_code, "as.data.frame", new_line = FALSE)
     return(df_code)
   }
-  construct_apply(x, fun = "data.frame", ...)
+  if (read.table && !any(lengths(lapply(x, attributes)))) {
+    code_df <- x
+    code_df[] <- lapply(x, as.character)
+    dbl_cols <- sapply(x, is.double)
+    code_df[dbl_cols] <- lapply(code_df[dbl_cols], function(col) sub("^(\\d+)$", "\\1.", col))
+    code_df <- rbind(names(x), code_df)
+    rn <- rownames(x)
+    if (is.character(attr(x, "row.names"))) code_df <- cbind(c("", sprintf("'%s'", rownames(x))), code_df)
+    code_df[] <- lapply(code_df, format)
+    code <- c("read.table(header = TRUE, text = \"", do.call(paste, code_df), "\")")
+    code
+    return(code)
+  }
+
+  construct_apply(x, fun = "data.frame", read.table = read.table, ...)
 }
 
 #' @export
