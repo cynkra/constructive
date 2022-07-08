116a82c6ee44b4494f7450bd003a98e646ae428a
Antoine Fabri <antoine.fabri@gmail.com>
2022-07-09 01:17:38 +0200

    Forward pipe arg correctly in dm methods

--- R/dm.R
+++ R/dm.R
@@ -8,7 +8,7 @@ construct_idiomatic.dm <- function(x, pipe = "base", ...) {
   pk_code <- unlist(Map(
     function(table, pk_tibble) {
       if (!nrow(pk_tibble)) return(character())
-      column_code <- construct_raw(pk_tibble$column[[1]], ...)
+      column_code <- construct_raw(pk_tibble$column[[1]], pipe = pipe, ...)
       paste0("dm::dm_add_pk(", protect(table), ", ", paste(column_code, collapse = "\n") , ")")
     } ,
     def$table,
@@ -26,9 +26,9 @@ construct_idiomatic.dm <- function(x, pipe = "base", ...) {
         paste0(
           "dm::dm_add_fk(",
           protect(table), ", ",
-          paste(construct_raw(column, ...), collapse = "\n"), ",",
+          paste(construct_raw(column, pipe = pipe, ...), collapse = "\n"), ",",
           protect(ref_table), ", ",
-          paste(construct_raw(ref_column, ...), collapse = "\n"), ")"
+          paste(construct_raw(ref_column, pipe = pipe, ...), collapse = "\n"), ")"
         )
       },
       fk_tibble$table,
@@ -46,7 +46,7 @@ construct_idiomatic.dm <- function(x, pipe = "base", ...) {
 
   colors <- set_names(def$table, def$display)[!is.na(def$display)]
   if (length(colors)) {
-    color_code <- construct_apply(colors, "dm::dm_set_colors", ...)
+    color_code <- construct_apply(colors, "dm::dm_set_colors", pipe = pipe, ...)
     code <- pipe(code, color_code, pipe)
   }
 
