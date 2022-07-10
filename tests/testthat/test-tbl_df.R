365d4d911afc2c2032beb4e7c2d68ff3ad65e6a5
Antoine Fabri <antoine.fabri@gmail.com>
2022-07-10 19:41:03 +0200

    Add tbl_df test for tribble = TRUE

--- tests/testthat/test-tbl_df.R
+++ tests/testthat/test-tbl_df.R
@@ -2,8 +2,8 @@ test_that("tbl_df", {
   sys_time_1970 <- Sys.time()
   sys_time_1970[[1]] <- 0
   expect_snapshot({
-    # ordered
     construct(dplyr::band_members)
+    construct(dplyr::band_members, tribble = TRUE)
     construct(dplyr::group_by(dplyr::band_members, band))
   })
 })
