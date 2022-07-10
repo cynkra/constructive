e788fe99dd94479cf767bb357d59459618a30793
Antoine Fabri <antoine.fabri@gmail.com>
2022-07-10 19:20:58 +0200

    Implement method for numeric to adress corner cases

---
+++ R/numeric.R
@@ -0,0 +1,31 @@
+
+# this is necessary because `dput()`, used for default the method, sometimes cuts values too short,
+# however this gives ugly values in the general case
+# in the 2 following cases we want the shortest "equal" output
+# format(5.1, digits = 22) # "5.099999999999999644729"
+# format(1e24, digits = 22) # 999999999999999983222784
+
+# => so we use `digits = 16` for the default since it seems to simplify those values,
+#    and we fall back on `digits = 22` for other cases
+
+# It doesn't fix everything, but this might be because of a system dependent bug:
+# In the following case it's worse, the output is NOT equal to the input
+# format(1e25, digits = 22) # 999999999999999983222784
+# we want to use only as many digits as necessary
+
+# all the above for :
+# R version 4.1.3 (2022-03-10)
+# Platform: aarch64-apple-darwin20 (64-bit)
+# Running under: macOS Monterey 12.0.1
+
+#' @export
+construct_idiomatic.double <- function(x, ...) {
+  format_flex <- function(x) {
+    formatted <- format(x, digits = 16)
+    if (as.numeric(formatted) == x) return(formatted)
+    format(x, digits = 22)
+  }
+  if (length(x) == 1) return(format_flex(x))
+  construct_apply(vapply(x, format_flex, character(1)), "c", new_line = FALSE, language = TRUE)
+}
+
