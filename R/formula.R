32127970031f9c422d74346e47bf3e86ab44d9e5
Antoine Fabri <antoine.fabri@gmail.com>
2022-07-10 19:55:02 +0200

    Remove unused corner case

--- R/formula.R
+++ R/formula.R
@@ -2,7 +2,6 @@
 construct_idiomatic.formula <- function(x, ...) {
   env <- environment(x)
   x_chr <- deparse(x)
-  if (is.null(env)) return(deparse(x_chr))
   env_chr <- construct_raw(env, ...)
   construct_apply(list(x_chr, env_chr), "rlang::set_env", language = TRUE, ...)
 }
