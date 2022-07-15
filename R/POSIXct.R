227f65511e64738a015e87228430eea444cf592c
Andrea Ranzato <andrea@cynkra.com>
2022-07-15 13:07:58 +0200

    as.POSIXct(character(0)) |> structure(tzone = NULL)

--- R/POSIXct.R
+++ R/POSIXct.R
@@ -1,6 +1,15 @@
 #' @export
 construct_idiomatic.POSIXct <- function(x, max_atomic = NULL, ...) {
-  max_atomic <- 0
+  browser()
+  if (length(x) == 0 || (!is.null(max_atomic) && max_atomic == 0)) {
+    # should I arrive at this point with length(x) already 0?
+    # I don't think so because here we have to retrieve other attributes,
+    # it is a more complex case compared with numeric(0)
+
+    # do something with x...
+    x <- x[0]
+  }
+
   tzone <- attr(x, "tzone")
   x_chr <- format(x)
   split_s <- as.numeric(x) %% 1
@@ -9,18 +18,13 @@ construct_idiomatic.POSIXct <- function(x, max_atomic = NULL, ...) {
   args <- list(x_chr)
   if (!is.null(tzone) && tzone != "") {
     args <- c(args, list(tz = tzone))
-  } else {
-    if (!is.null(max_atomic)) {
-      if (max_atomic == 0) {
-        args <- list(split_s)
-      }
-    }
   }
   construct_apply(args, "as.POSIXct", new_line = TRUE)
 }
 
 #' @export
 repair_attributes.POSIXct <- function(x, code, pipe ="base", ...) {
+  browser()
   repair_attributes_impl(
     x, code, pipe,
     idiomatic_class = c("POSIXct", "POSIXt"),
