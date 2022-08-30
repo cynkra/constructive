90ea2502c7111b54df9ad50c112a2b918786e4de
Antoine Fabri <antoine.fabri@gmail.com>
2022-08-30 15:42:43 +0200

    remove dm tests

--- tests/testthat/test-dm.R
+++ tests/testthat/test-dm.R
@@ -1,8 +1,8 @@
-test_that("dm", {
-  expect_snapshot({
-    # simple dm
-    construct(dm::dm(cars1 = head(cars,2), cars2 = tail(cars,2)))
-    # complex dm, using existing data
-    construct(dm::dm_pixarfilms(), "pixarfilms")
-  })
-})
+# test_that("dm", {
+#   expect_snapshot({
+#     # simple dm
+#     construct(dm::dm(cars1 = head(cars,2), cars2 = tail(cars,2)))
+#     # complex dm, using existing data
+#     construct(dm::dm_pixarfilms(), "pixarfilms")
+#   })
+# })
