593558b002124a0d3bab9de31ed0b617c335b970
Antoine Fabri <antoine.fabri@gmail.com>
2022-09-03 11:37:16 +0200

    Deal with srcrefs and improve attribute handling

--- R/language.R
+++ R/language.R
@@ -1,6 +1,7 @@
 #' @export
 construct_idiomatic.language <- function(x, ...) {
   if (identical(x, quote(expr=))) return("quote(expr=)")
+  attributes(x) <- NULL
   if (is_expression2(x)) {
     wrap(rlang::expr_deparse(x), "quote", new_line = FALSE)
   } else {
@@ -19,7 +20,12 @@ repair_attributes.language <- function(x, code, pipe ="base", ...) {
 }
 
 is_expression2 <- function(x) {
+  non_srcref_attr_nms <-  setdiff(
+    names(attributes(x)),
+    c("srcref", "srcfile", "wholeSrcref")
+  )
+  if(length(non_srcref_attr_nms)) return(FALSE)
   if (rlang::is_syntactic_literal(x) || rlang::is_symbol(x)) return(TRUE)
   if(!rlang::is_call(x)) return(FALSE)
-  all(vapply(x, function(x) is.null(attributes(x)) && is_expression2(x), logical(1)))
+  all(vapply(x, is_expression2, logical(1)))
 }
