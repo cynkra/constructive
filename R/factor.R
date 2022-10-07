172cd5d005560a8f62703ee5b07ccb0326fe2cbe
Antoine Fabri <antoine.fabri@gmail.com>
2022-10-07 11:25:31 +0200

    Constructive options for class 'factor'

--- R/factor.R
+++ R/factor.R
@@ -1,12 +1,52 @@
+#' Constructive options for class 'factor'
+#'
+#' These options will be used on objects of class 'factor'.
+#'
+#' Depending on `constructor`, we construct the environment as follows:
+#' * `"factor"` (default): Build the object using a `factor()` call, levels won't
+#'   be defined explicitly if they are in alphabetical order (locale dependent!)
+#' * `"as_factor"` : Build the object using a `forcats::as_factor()` call whenever
+#'   possible, levels won't be defined explicitly if they are in the order of appearance.
+#'   back to `data.frame()`.
+#' * `"new_factor"` : Build the object using a `vctrs::new_factor()` call. Levels are
+#'   always defined explicitly.
+#'
+#' @param constructor String. Name of the function used to construct the environment, see Details section.
+#'
+#' @return An object of class <constructive_options/constructive_options_factor>
+#' @export
+opts_factor <- function(constructor = c("factor", "as_factor", "new_factor")) {
+  constructor <- rlang::arg_match(constructor)
+  structure(
+    class = c("constructive_options", "constructive_options_factor"),
+    list(constructor = constructor)
+  )
+}
+
 #' @export
 construct_idiomatic.factor <- function(x, ...) {
-  default_levs <- sort(unique(as.character(x)))
+  args <- fetch_opts("factor", ...)
+  constructor <- args$constructor
   levs <- levels(x)
-  args <- if (identical(default_levs, levs)) {
-    construct_apply(list(levs[x]), "factor", new_line =  FALSE, ...)
-  } else {
-    construct_apply(list(levs[x], levels = levs), "factor", ...)
+
+  if (constructor == "new_factor") {
+    code <- construct_apply(list(as.integer(x), levels = levs), "vctrs::new_factor", ...)
+    return(code)
   }
+
+  if (constructor == "as_factor" && identical(unique(as.character(x)), levs)) {
+    code <- construct_apply(list(levs[x]), "forcats::as_factor", new_line =  FALSE, ...)
+    return(code)
+  }
+
+  # constructor == "factor"
+  default_levs <- sort(unique(as.character(x)))
+  if (identical(default_levs, levs)) {
+    code <- construct_apply(list(levs[x]), "factor", new_line =  FALSE, ...)
+  } else {
+    code <- construct_apply(list(levs[x], levels = levs), "factor", ...)
+  }
+  code
 }
 
 #' @export
--- tests/testthat/_snaps/factor.md
+++ tests/testthat/_snaps/factor.md
@@ -8,7 +8,7 @@
         "Oct", "Nov", "Dec"
       ))
     Code
-      construct(factor(month.abb, rev(month.abb)))
+      construct(factor(month.abb, month.abb))
     Output
       factor(
         c(
@@ -16,8 +16,42 @@
           "Oct", "Nov", "Dec"
         ),
         levels = c(
-          "Dec", "Nov", "Oct", "Sep", "Aug", "Jul", "Jun", "May", "Apr",
-          "Mar", "Feb", "Jan"
+          "Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep",
+          "Oct", "Nov", "Dec"
+        )
+      )
+    Code
+      construct(factor(month.abb), opts_factor("as_factor"))
+    Output
+      factor(c(
+        "Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep",
+        "Oct", "Nov", "Dec"
+      ))
+    Code
+      construct(factor(month.abb, month.abb), opts_factor("as_factor"))
+    Output
+      forcats::as_factor(c(
+        "Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep",
+        "Oct", "Nov", "Dec"
+      ))
+    Code
+      construct(factor(month.abb), opts_factor("new_factor"))
+    Output
+      vctrs::new_factor(
+        c(5L, 4L, 8L, 1L, 9L, 7L, 6L, 2L, 12L, 11L, 10L, 3L),
+        levels = c(
+          "Apr", "Aug", "Dec", "Feb", "Jan", "Jul", "Jun", "Mar", "May",
+          "Nov", "Oct", "Sep"
+        )
+      )
+    Code
+      construct(factor(month.abb, month.abb), opts_factor("new_factor"))
+    Output
+      vctrs::new_factor(
+        1:12,
+        levels = c(
+          "Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep",
+          "Oct", "Nov", "Dec"
         )
       )
 
