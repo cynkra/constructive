419e62ec6e5cfdaac7a6a4c25c419c0f7d954065
Antoine Fabri <antoine.fabri@gmail.com>
2022-10-11 17:05:21 +0200

    Fix construct_apply for 0 length args

--- R/generic_and_default.R
+++ R/generic_and_default.R
@@ -67,7 +67,7 @@ repair_attributes_impl <- function(x, code, ..., pipe = "base", ignore = NULL, i
 construct_apply <- function(args, fun = "list", ..., keep_trailing_comma = FALSE, language = FALSE, implicit_names = FALSE, new_line = TRUE, one_liner = FALSE) {
   new_line <- new_line && !one_liner
   keep_trailing_comma <- keep_trailing_comma && !one_liner
-  if (!length(args)) return("list()")
+  if (!length(args)) return(sprintf("%s()", fun))
   if (!language) args <- lapply(args, construct_raw, ..., one_liner = one_liner)
   args_chr <- Map(name_and_append_comma, args, names2(args), implicit_names = implicit_names)
   args_chr <- unlist(args_chr)
