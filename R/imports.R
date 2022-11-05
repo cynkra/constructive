4556fdac94c0a68cd4e4d2c73b8de246d69e0938
Antoine Fabri <antoine.fabri@gmail.com>
2022-11-05 11:06:23 +0100

    import enquo enquos sym

--- R/imports.R
+++ R/imports.R
@@ -1,4 +1,4 @@
-#' @importFrom rlang abort warn try_fetch set_names names2 caller_env %||%
+#' @importFrom rlang abort warn try_fetch set_names names2 caller_env %||% enquo enquos sym
 #' @importFrom utils capture.output installed.packages head tail
 #' @importFrom stats setNames
 NULL
