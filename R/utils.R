084f8718a28eaf8d03dda61113024f7858980d46
Antoine Fabri <antoine.fabri@gmail.com>
2022-11-06 21:06:05 +0100

    improve namespace_as_list() so names are namespaced

--- R/utils.R
+++ R/utils.R
@@ -34,10 +34,12 @@ protect <- function(name) {
 namespace_as_list <- function(pkg) {
   ns <- asNamespace(pkg)
   if (pkg == "base") return(as.list(ns))
-  c(
+  objs <- c(
     mget(getNamespaceExports(ns), ns, inherits = TRUE, ifnotfound = list(NULL)),
     as.list(.getNamespaceInfo(ns, "lazydata"))
   )
+  names(objs) <- paste0(pkg, "::", names(objs))
+  objs
 }
 
 # much faster than match()
