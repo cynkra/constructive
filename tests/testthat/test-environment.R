15009e6b69846d8d0e5b902d1627de8c22fa6ffc
Antoine Fabri <antoine.fabri@gmail.com>
2022-11-10 00:00:56 +0100

    test `predefine`

--- tests/testthat/test-environment.R
+++ tests/testthat/test-environment.R
@@ -28,5 +28,13 @@ test_that("environment", {
     construct(e2, opts_environment(constructor = "new.env"))
     construct(e2, opts_environment(constructor = "topenv"))
     construct(e2, opts_environment(constructor = "as.environment"))
+    construct(tidyselect::peek_vars, opts_environment(predefine = TRUE), opts_function(environment = TRUE))
+    # circularity
+    evalq({
+    e <- new.env()
+    e$f <- e
+    foo <- evalq(~a, e)
+    construct(foo, opts_environment(predefine = TRUE), opts_formula(environment = TRUE))
+    }, .GlobalEnv)
   })
 })
