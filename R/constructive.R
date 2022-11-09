2d3020c841015b6d041983206c550141f3697599
Antoine Fabri <antoine.fabri@gmail.com>
2022-11-09 23:57:40 +0100

    Implement `predefine`

--- R/constructive.R
+++ R/constructive.R
@@ -48,6 +48,7 @@
 construct <- function(x, ..., data = NULL, pipe = c("base", "magrittr"), check = NULL,
                       ignore_srcref = TRUE, ignore_attr = FALSE, ignore_function_env = FALSE, ignore_formula_env = FALSE, one_liner = FALSE,
                       template = getOption("constructive_opts_template")) {
+  # reset globals
   globals$predefinition <- character()
   globals$envs <- data.frame(hash = character(), name = character())
 
