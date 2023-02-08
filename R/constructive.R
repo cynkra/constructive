f37d6ce2bcd4649d1817e24de5897d5cfc7afa10
Antoine Fabri <antoine.fabri@gmail.com>
2023-02-08 10:49:05 +0100

    Fix encoding issue

--- R/constructive.R
+++ R/constructive.R
@@ -66,6 +66,7 @@ construct <- function(x, ..., data = NULL, pipe = c("base", "magrittr"), check =
   data <- preprocess_data(data)
   code <- try_construct(x, template = template, ..., data = data, pipe = pipe, one_liner = one_liner)
   code <- c(globals$predefinition, code)
+  Encoding(code) <- "UTF-8"
   styled_code <- try_parse(code, data, one_liner)
   caller <- caller_env()
   compare <- check_round_trip(x, styled_code, data, check, ignore_srcref, ignore_attr, ignore_function_env, ignore_formula_env, caller)
@@ -99,6 +100,7 @@ construct_multi <- function(x, ..., data = NULL, pipe = c("base", "magrittr"), c
       c(x, "")
     })
   code <- unlist(code)
+  Encoding(code) <- "UTF-8"
   if (is.null(code)) code <- character(0)
   class(code) <- "vertical"
   structure(list(code = unname(code), compare = issues), class = "constructive")
