a254af95eadff975215dc2b13ec88f6dfd4af892
Antoine Fabri <antoine.fabri@gmail.com>
2023-03-29 12:13:11 +0200

    handle externalptr

---
+++ src/constructive.c
@@ -0,0 +1,18 @@
+#include <R.h>
+#include <Rinternals.h>
+#include <Rdefines.h>
+#include <stdio.h>
+
+
+SEXP external_pointer(SEXP p, SEXP tag, SEXP protected) {
+  return R_MakeExternalPtr((void*) strtol(CHAR(STRING_PTR(p)[0]), NULL, 0), tag, protected);
+}
+
+SEXP external_pointer_address(SEXP s) {
+  if (TYPEOF(s) != EXTPTRSXP) {
+    error("external_pointer_address() expects an input of type 'externalptr'");
+  }
+  char* buf[20];
+  sprintf((char*) buf, "%p", R_ExternalPtrAddr(s));
+  return Rf_mkString((char*) buf);
+}
--- tests/testthat/test-externalptr.R
+++ tests/testthat/test-externalptr.R
@@ -1,6 +1,7 @@
 test_that("externalptr", {
   expect_snapshot({
-    obj <- attributes(data.table::data.table(a=1))
-    construct(obj, check = FALSE)
+    dt <- data.table::data.table(a = 1)
+    class(dt) <- "data.frame"
+    construct(dt)
   })
 })
