28ad1c8afdde67c61d13e40661d7ed5c05526925
Antoine Fabri <antoine.fabri@gmail.com>
2023-03-31 11:42:28 +0200

    Extract construct_issues.R

--- R/construct.R
+++ R/construct.R
@@ -112,18 +112,6 @@ construct_multi <- function(x, ..., data = NULL, pipe = c("base", "magrittr"), c
 
 globals <- new.env()
 
-#' Show constructive issues
-#'
-#' @param x An object built by `construct()`, if `NULL` the latest encountered
-#'   issues will be displayed
-#'
-#' @return A character vector with class "waldo_compare"
-#' @export
-construct_issues <- function(x = NULL) {
-  if (is.null(x)) return(globals$issues)
-  x$compare
-}
-
 #' @export
 print.constructive <- function(x, ...) {
   print(x$code)
---
+++ R/construct_issues.R
@@ -0,0 +1,11 @@
+#' Show constructive issues
+#'
+#' @param x An object built by `construct()`, if `NULL` the latest encountered
+#'   issues will be displayed
+#'
+#' @return A character vector with class "waldo_compare"
+#' @export
+construct_issues <- function(x = NULL) {
+  if (is.null(x)) return(globals$issues)
+  x$compare
+}
