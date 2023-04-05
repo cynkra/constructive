cadaa2606df2d13031623f942566a48fb70f543d
Antoine Fabri <antoine.fabri@gmail.com>
2023-04-05 17:26:21 +0200

    import utils::getFromNamespace

--- R/constructive-package.R
+++ R/constructive-package.R
@@ -7,7 +7,7 @@ globals$ggpackages <- "ggplot2"
 "_PACKAGE"
 
 #' @import rlang
-#' @importFrom utils capture.output installed.packages head tail
+#' @importFrom utils capture.output installed.packages head tail getFromNamespace
 #' @importFrom stats setNames
 NULL
 
