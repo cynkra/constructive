440e66f568abb4dc4bcf91dcff75665687ace453
Antoine Fabri <antoine.fabri@gmail.com>
2023-04-05 17:20:26 +0200

    Fix CRAN note due to low level code

--- src/constructive.c
+++ src/constructive.c
@@ -3,6 +3,26 @@
 #include <Rdefines.h>
 #include <stdio.h>
 #include <inttypes.h>
+#include <stdlib.h> // for NULL
+#include <R_ext/Rdynload.h>
+
+/* .Call calls */
+extern SEXP external_pointer(SEXP);
+extern SEXP external_pointer_address(SEXP);
+extern SEXP objectFromAddress(SEXP);
+
+static const R_CallMethodDef CallEntries[] = {
+  {"external_pointer",         (DL_FUNC) &external_pointer,         1},
+  {"external_pointer_address", (DL_FUNC) &external_pointer_address, 1},
+  {"objectFromAddress",        (DL_FUNC) &objectFromAddress,        1},
+  {NULL, NULL, 0}
+};
+
+void R_init_constructive(DllInfo *dll)
+{
+  R_registerRoutines(dll, NULL, CallEntries, NULL, NULL);
+  R_useDynamicSymbols(dll, FALSE);
+}
 
 // Thanks to Randi Lai: https://github.com/randy3k/xptr/
 SEXP external_pointer(SEXP p) {
