fbb79073763f658ca719e4f0be12b11f2c456434
Antoine Fabri <antoine.fabri@gmail.com>
2023-04-05 17:28:30 +0200

    env -> envir and name arg to avoid collision and partial matching in update_predefinition()

--- R/s3-environment.R
+++ R/s3-environment.R
@@ -105,7 +105,7 @@ construct_idiomatic.environment <- function(x, ..., pipe = "base", one_liner = F
     globals$special_envs <-  c(row.names(installed.packages()), search(), "R_EmptyEnv", "R_GlobalEnv")
     # construct only if not found
     if (!environmentName(x) %in% globals$special_envs) {
-      code <- update_predefinition(x, ..., pipe = pipe, one_liner = one_liner)
+      code <- update_predefinition(envir = x, ..., pipe = pipe, one_liner = one_liner)
       return(code)
     }
   }
@@ -252,11 +252,11 @@ env <- function(address, parents = NULL) {
   env_impl(address)
 }
 
-update_predefinition <- function(env, ...) {
+update_predefinition <- function(envir, ...) {
   # construct parent before constructing env
-  parent_code <- construct_raw(parent.env(env), ...)
-  # if env was already constructed (recorded in globals$env), just return its name
-  hash <- format(env)
+  parent_code <- construct_raw(parent.env(envir), ...)
+  # if envir was already constructed (recorded in globals$env), just return its name
+  hash <- format(envir)
   if (hash %in% globals$envs$hash) {
     return(globals$envs$name[hash == globals$envs$hash][[1]])
   }
@@ -267,16 +267,16 @@ update_predefinition <- function(env, ...) {
   # initialize with new.env(), repairing attributes
   code <- sprintf("new.env(parent = %s)", parent_code)
   # hack: we use  opts_environment(predefine = FALSE) to switch on attribute reparation just there
-  code <- repair_attributes.environment(env, code, opts_environment(predefine = FALSE), ...)
+  code <- repair_attributes.environment(envir, code, opts_environment(predefine = FALSE), ...)
   code[[1]] <- sprintf("%s <- %s", env_name, code[[1]])
-  # update predefinitions with env definition
+  # update predefinitions with envir definition
   globals$predefinition <- c(
     globals$predefinition,
     code
   )
-  # build non environment objects of env above
-  for(nm in names(env)) {
-    obj <- env[[nm]]
+  # build non environment objects of envir above
+  for(nm in names(envir)) {
+    obj <- envir[[nm]]
     if (missing(obj)) {
       obj_code <- sprintf("%s$%s <- quote(expr=)", env_name, nm)
       globals$predefinition <- c(
@@ -295,9 +295,9 @@ update_predefinition <- function(env, ...) {
     }
   }
 
-  # build environment objects of env above
-  for(nm in names(env)) {
-    obj <- env[[nm]]
+  # build environment objects of envir above
+  for(nm in names(envir)) {
+    obj <- envir[[nm]]
     if (missing(obj)) {
       obj_code <- sprintf("%s$%s <- quote(expr=)", env_name, nm)
       globals$predefinition <- c(
