44f7c57f225644b405b9b75c1e8ce492fd6f22c9
Antoine Fabri <antoine.fabri@gmail.com>
2023-04-05 05:35:36 +0200

    construct_signature works with non syntactic arguments

--- R/roxygen2-tags.R
+++ R/roxygen2-tags.R
@@ -55,6 +55,8 @@ construct_signature <- function(x, name = NULL, one_liner = FALSE, style = TRUE)
   empty_lgl <- sapply(fun_lst, identical, quote(expr=))
   fun_lst[empty_lgl] <- lapply(names(fun_lst)[empty_lgl], as.symbol)
   names(fun_lst)[empty_lgl] <- ""
+  names(fun_lst)[!empty_lgl] <- protect(names(fun_lst)[!empty_lgl])
+  name <- protect(name)
   signature_lng <- as.call(c(as.symbol(name), fun_lst[-length(fun_lst)]))
   deparse_call(signature_lng, one_liner = one_liner, style = style)
 }
