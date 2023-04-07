9e55113be72b884c1a571cb42fe77f933ffe386d
Antoine Fabri <antoine.fabri@gmail.com>
2023-04-07 16:03:39 +0200

    update ggplot test

--- tests/testthat/test-s3-ggplot2-ggplot.R
+++ tests/testthat/test-s3-ggplot2-ggplot.R
@@ -4,10 +4,14 @@ test_that("ggplot", {
     library(ggplot2, warn = FALSE)
     mpg_99 <- mpg %>% filter(year == 1999)
     base_99 <- ggplot(mpg_99, aes(displ, hwy)) + geom_point()
-    p <-
-      base_99 +
-      scale_x_continuous(limits = c(1, 7)) +
-      scale_y_continuous(limits = c(10, 45))
-    construct(p, data = lst(mpg_99), check = FALSE)
+
+    construct(base_99, data = lst(mpg_99), opts_Layer("layer"))
+    construct(base_99, data = lst(mpg_99), opts_Layer("environment"), check = FALSE)
+
+    p1 <- base_99  + scale_x_continuous(limits = c(1, 7))
+    construct(p1, data = lst(mpg_99))
+
+    p2 <- p1 + scale_y_continuous(limits = c(10, 45)) + facet_wrap(~manufacturer)
+    construct(p2, data = lst(mpg_99))
   })
 })
