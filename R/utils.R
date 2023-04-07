6fc066c4a808c58fbc77dc98d7465c3a18c17833
Antoine Fabri <antoine.fabri@gmail.com>
2023-04-07 13:53:01 +0200

    Remove obsolete functions

--- R/utils.R
+++ R/utils.R
@@ -67,69 +67,6 @@ flex_match <- function(needle, haystack) {
   if (any(ind)) names(haystack[ind])[1]
 }
 
-
-# adapted from glue::glue_collapse
-collapse <- function (x, sep = ",", width = 80, last = " and ", quote = "") {
-  if (length(x) == 0) {
-    return(character())
-  }
-  if (any(is.na(x))) {
-    return(NA_character_)
-  }
-  x <- paste0(quote, x, quote)
-  if (nzchar(last) && length(x) > 1) {
-    res <- collapse(x[seq(1, length(x) - 1)], sep = sep, width = Inf, last = "")
-    return(collapse(paste0(res, last, x[length(x)]), width = width))
-  }
-  x <- paste0(x, collapse = sep)
-  if (width < Inf) {
-    x_width <- nchar(x, "width")
-    too_wide <- x_width > width
-    if (too_wide) {
-      x <- paste0(substr(x, 1, width - 3), "...")
-    }
-  }
-  x
-}
-
-with_s3_method <- function(
-    genname,
-    class,
-    method,
-    envir,
-    expr
-) {
-  if (identical(Sys.getenv("TESTTHAT"), "true")) return(eval(substitute(expr), parent.frame()))
-  ub <- match.fun("unlockBinding")
-  ns <- asNamespace(envir)
-  method_name <- paste0(genname, ".", class)
-  method_exists <- exists(method_name, ns$.__S3MethodsTable__.)
-  on.exit({
-    if (method_exists) {
-      ns$.__S3MethodsTable__.[[method_name]] <- initial_method
-    } else {
-      ns$.__NAMESPACE__.$S3methods <-
-        ns$.__NAMESPACE__.$S3methods[ns$.__NAMESPACE__.$S3methods[,3] != method_name]
-      lockBinding(".__NAMESPACE__.", ns)
-      rm(list = method_name, envir = ns$.__S3MethodsTable__.)
-    }
-    lockBinding(".__S3MethodsTable__.", ns)
-  })
-  if(method_exists) {
-    initial_method <- ns$.__S3MethodsTable__.[[method_name]]
-  } else {
-    # edit S3 table
-    ub(".__NAMESPACE__.", ns)
-    ns$.__NAMESPACE__.$S3methods <- rbind(
-      ns$.__NAMESPACE__.$S3methods,
-      c(genname, class, method, NA_character_)
-    )
-  }
-  ub(".__S3MethodsTable__.", ns)
-  ns$.__S3MethodsTable__.[[method_name]] <- method
-  eval(substitute(expr), parent.frame())
-}
-
 compare_proxy_ggplot <- function(x, path) {
   x <- rapply(x, function(x) {if (!is.environment(x)) return(x) else rlang::env_clone(x)}, how = "replace")
   for (i in seq_along(x$layers)) {
