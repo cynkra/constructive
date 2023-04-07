933f05d1619c3794a0e633328c1b28510f68d0b4
Antoine Fabri <antoine.fabri@gmail.com>
2023-04-07 13:51:50 +0200

    Implement more methods for themes

--- R/s3-ggplot2-theme.R
+++ R/s3-ggplot2-theme.R
@@ -64,3 +64,60 @@ construct_idiomatic.element_text <- function(x, ...) {
 repair_attributes.element_text <- function(x, ...) {
   repair_attributes_impl(x, idiomatic_class = c("element_text", "element"), ...)
 }
+
+#' @export
+construct_idiomatic.element_line <- function(x, ...) {
+  args <- keep_only_non_defaults(unclass(x), ggplot2::element_line)
+  construct_apply(args, "ggplot2::element_line", ...)
+}
+
+#' @export
+repair_attributes.element_line <- function(x, ...) {
+  repair_attributes_impl(x, idiomatic_class = c("element_line", "element"), ...)
+}
+
+#' @export
+construct_idiomatic.simpleUnit <- function(x, ...) {
+  lkp <- c(
+    npc = 0L, cm = 1L, inches = 2L, mm = 7L, points = 8L, picas = 9L,
+    bigpts = 10L, dida = 11L, cicero = 12L, scaledpts = 13L, lines = 3L,
+    char = 18L, native = 4L, snpc = 6L, mylines = 103L, mychar = 104L,
+    mystrwidth = 105L, mystrheight = 106L
+  )
+  units <- names(lkp)[match(attr(x, "unit"), lkp)]
+  x <- as.vector(x)
+  construct_apply(list(x, units = units), "grid::unit", ...)
+}
+
+#' @export
+repair_attributes.simpleUnit <- function(x, ...) {
+  repair_attributes_impl(x, idiomatic_class = c("simpleUnit", "unit", "unit_v2"), ignore = "unit", ...)
+}
+
+#' @export
+construct_idiomatic.margin <- function(x, ...) {
+  lkp <- c(
+    npc = 0L, cm = 1L, inches = 2L, mm = 7L, points = 8L, picas = 9L,
+    bigpts = 10L, dida = 11L, cicero = 12L, scaledpts = 13L, lines = 3L,
+    char = 18L, native = 4L, snpc = 6L, mylines = 103L, mychar = 104L,
+    mystrwidth = 105L, mystrheight = 106L
+  )
+  unit <- names(lkp)[match(attr(x, "unit"), lkp)]
+  x <- as.vector(x)
+  construct_apply(list(t = x[1], r = x[2], b = x[3], l = x[4], unit = unit), "ggplot2::margin", ...)
+}
+
+#' @export
+repair_attributes.margin <- function(x, ...) {
+  repair_attributes_impl(x, idiomatic_class = c("margin", "simpleUnit", "unit", "unit_v2"), ignore = "unit", ...)
+}
+
+#' @export
+construct_idiomatic.rel <- function(x, ...) {
+  construct_apply(list(unclass(x)), "ggplot2::rel", ...)
+}
+
+#' @export
+repair_attributes.rel <- function(x, ...) {
+  repair_attributes_impl(x, idiomatic_class = "rel", ...)
+}
