515d53633f3929ac8ee5ddbc15a9cf039601d52d
Kirill Müller <krlmlr+r@mailbox.org>
2023-04-08 17:13:00 +0200

    Always non-interactive

--- tests/testthat/test-construct_diff.R
+++ tests/testthat/test-construct_diff.R
@@ -2,7 +2,8 @@ test_that("construct_diff", {
   expect_snapshot({
     construct_diff(
       list(a = head(cars,2), b = "aaaaaaaaaaaaaaaaaaaa", c = "Foo"),
-      list(a = head(iris,1), b = "aaaaaaaaaaaaaaaaaaaa", c = "foo")
+      list(a = head(iris,1), b = "aaaaaaaaaaaaaaaaaaaa", c = "foo"),
+      interactive = FALSE
     )
 
     construct_diff(
