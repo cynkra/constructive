3465ccb77d03f30b3a4683fd5e6411690744b54e
Antoine Fabri <antoine.fabri@gmail.com>
2023-04-16 16:23:41 +0200

    Improve support of themes

--- tests/testthat/test-s3-ggplot2-theme.R
+++ tests/testthat/test-s3-ggplot2-theme.R
@@ -1,5 +1,11 @@
 test_that("theme", {
-  expect_snapshot(
+  expect_snapshot({
     construct(ggplot2::theme_bw())
-  )
+    construct(ggplot2::theme_bw(base_size = 11, base_line_size = 0.5))
+    construct(ggplot2::theme_bw(base_size = 22, base_line_size = 0.5))
+    construct(ggplot2::theme_bw(base_size = 11, base_line_size = 1))
+    construct(ggplot2::theme_bw(base_size = 22, base_line_size = 1))
+    construct(ggplot2::theme())
+    construct(ggplot2::theme(axis.title = "foo"))
+  })
 })
