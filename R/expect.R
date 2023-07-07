d381277399561f5cdd118dbb8594362f7aad82ac
Kirill Müller <krlmlr+r@mailbox.org>
2023-07-07 20:36:57 +0200

    Qualify

--- R/expect.R
+++ R/expect.R
@@ -1,5 +1,5 @@
 expect_pipe_snapshot <- function(code) {
-  rlang::inject(expect_snapshot({{ code }}, transform = function(x) {
+  rlang::inject(testthat::expect_snapshot({{ code }}, transform = function(x) {
     x <- gsub("%>%", "|>", x, fixed = TRUE)
     x <- gsub("= [.]([,)])", "= _\\1", x)
     x
