133e4c032e69eb0b690950cc80a85c1af08b4751
Kirill Müller <kirill@cynkra.com>
2023-11-03 18:24:22 +0100

    Use :: consistently in tests

--- tests/testthat/test-s3-ggplot2-ggplot.R
+++ tests/testthat/test-s3-ggplot2-ggplot.R
@@ -1,17 +1,15 @@
 test_that("ggplot", {
   expect_pipe_snapshot({
-    library(dplyr, warn = FALSE)
-    library(ggplot2, warn = FALSE)
-    mpg_99 <- mpg %>% filter(year == 1999)
-    base_99 <- ggplot(mpg_99, aes(displ, hwy)) + geom_point()
+    mpg_99 <- ggplot2::mpg %>% dplyr::filter(year == 1999)
+    base_99 <- ggplot2::ggplot(mpg_99, ggplot2::aes(displ, hwy)) + ggplot2::geom_point()
 
-    construct(base_99, data = lst(mpg_99), opts_Layer("layer"), check = FALSE)
-    construct(base_99, data = lst(mpg_99), opts_Layer("environment"), check = FALSE)
+    construct(base_99, data = tibble::lst(mpg_99), opts_Layer("layer"), check = FALSE)
+    construct(base_99, data = tibble::lst(mpg_99), opts_Layer("environment"), check = FALSE)
 
-    p1 <- base_99  + scale_x_continuous(limits = c(1, 7))
-    construct(p1, data = lst(mpg_99), check = FALSE)
+    p1 <- base_99 + ggplot2::scale_x_continuous(limits = c(1, 7))
+    construct(p1, data = tibble::lst(mpg_99), check = FALSE)
 
-    p2 <- p1 + scale_y_continuous(limits = c(10, 45)) + facet_wrap(~manufacturer)
-    construct(p2, data = lst(mpg_99), check = FALSE)
+    p2 <- p1 + ggplot2::scale_y_continuous(limits = c(10, 45)) + ggplot2::facet_wrap(~manufacturer)
+    construct(p2, data = tibble::lst(mpg_99), check = FALSE)
   })
 })
