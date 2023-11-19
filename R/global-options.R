7129dc4a9cd50dfaf7a1f2e890097382ea324364
Antoine Fabri <antoine.fabri@gmail.com>
2023-11-19 19:32:10 +0100

    Update option documentation

--- R/global-options.R
+++ R/global-options.R
@@ -4,11 +4,13 @@
 #' permanently you can edit your `.RProfile` (`usethis::edit_r_profile()` might help).
 #'
 #' * Set `options(constructive_print = <character>)` where `<character>` is a vector
-#'     of strings among `"console"`, `"clipboard"` and `"reprex"`. The default
-#'     behavior is "console". If you use `"clipboard"`, with or without `"reprex"`,
-#'     the clipboard will contain only the code. If you use `"reprex"` without
-#'     `"clipboard"` the clipboard will still be used to receive the reprex,
-#'     can be tweaked with `options(reprex.clipboard = FALSE)`
+#'     of strings among the following :
+#'     * `"console"` : The default behavior, the code is printed in the console
+#'     * `"script"` : The code is copied to a new R script
+#'     * `"reprex"` : The code is shown in the viewer as a reprex,
+#'     the reprex (not only the code!) is also copied to the clipboard
+#'     * `"clipboard"` : The constructed code is copied to the clipboard, if combined
+#'     with `"reprex"` this takes precedence
 #' * Set `options(constructive_opts_template = <list>)` to set default constructive options,
 #'     see documentation of the `template` arg in `?construct`
 #' * Set `options(constructive_pretty = FALSE)` to disable pretty printinh using
