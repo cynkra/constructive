73b978c778f92371e989226b6813ec167015dafa
Antoine Fabri <antoine.fabri@gmail.com>
2023-11-23 23:11:05 +0100

    Harmonize margin

---
+++ R/s3-ggplot2-margin.R
@@ -0,0 +1,30 @@
+constructors$margin <- new.env()
+
+#' @export
+opts_margin <- new_constructive_opts_function("margin", "margin")
+
+#' @export
+.cstr_construct.margin <- new_constructive_method("margin", "margin")
+
+is_corrupted_margin <- function(x) {
+  # TODO
+  FALSE
+}
+
+#' @export
+constructors$margin$margin <- function(x, ...) {
+  lkp <- c(
+    npc = 0L, cm = 1L, inches = 2L, mm = 7L, points = 8L, picas = 9L,
+    bigpts = 10L, dida = 11L, cicero = 12L, scaledpts = 13L, lines = 3L,
+    char = 18L, native = 4L, snpc = 6L, mylines = 103L, mychar = 104L,
+    mystrwidth = 105L, mystrheight = 106L
+  )
+  unit <- names(lkp)[match(attr(x, "unit"), lkp)]
+  x <- as.vector(x)
+  code <- .cstr_apply(list(t = x[1], r = x[2], b = x[3], l = x[4], unit = unit), "ggplot2::margin", ...)
+  repair_attributes_margin(x, code, ...)
+}
+
+repair_attributes_margin <- function(x, ...) {
+  .cstr_repair_attributes(x, idiomatic_class = c("margin", "simpleUnit", "unit", "unit_v2"), ignore = "unit", ...)
+}
