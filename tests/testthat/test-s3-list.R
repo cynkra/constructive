608dc972109945d8df35b18c23ff20b93ca15914
Antoine Fabri <antoine.fabri@gmail.com>
2024-02-08 12:09:21 +0100

    test new behavior

--- tests/testthat/test-s3-list.R
+++ tests/testthat/test-s3-list.R
@@ -31,4 +31,13 @@ test_that("list", {
     construct(x2, opts_list(trim = 2, fill = "none"))
     construct(x2, opts_list(trim = 2, fill = "..."))
   })
+
+  corrupted_list <- structure(list(1), class = c("corrupted"))
+  length.corrupted <- function(x) stop()
+  `[.corrupted` <- function(...) stop()
+  `[[.corrupted` <- function(...) stop()
+  expect_error(length(corrupted_list))
+  expect_snapshot({
+    construct(corrupted_list)
+  })
 })
