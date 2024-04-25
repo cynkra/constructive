5494e1927cb7813748d6911e5aec8e1e790d2d74
Antoine Fabri <antoine.fabri@gmail.com>
2024-04-25 19:01:34 +0200

    simpleCondition

---
+++ R/s3-simpleCondition.R
@@ -0,0 +1,42 @@
+#' @export
+#' @rdname other-opts
+opts_simpleCondition <- function(constructor = c("simpleCondition", "next"), ...) {
+  .cstr_combine_errors(
+    constructor <- rlang::arg_match(constructor),
+    check_dots_empty()
+  )
+  .cstr_options("simpleCondition", constructor = constructor)
+}
+
+#' @export
+.cstr_construct.simpleCondition <- function(x, ...) {
+  opts <- .cstr_fetch_opts("simpleCondition", ...)
+  if (is_corrupted_simpleCondition(x) || opts$constructor == "next") return(NextMethod())
+  constructor <- constructors$simpleCondition[[opts$constructor]]
+  constructor(x, ...)
+}
+
+is_corrupted_simpleCondition <- function(x) {
+  !(is.list(x) && identical(names(x)[1:2], c("message", "call")))
+}
+
+#' @export
+constructors$simpleCondition$simpleCondition <- function(x, ...) {
+  # we let attributes to the reparation step
+  x_bkp <- x
+  x <- unclass(x)
+  if (is.null(x$call)) {
+    code <- .cstr_apply(list(x$message), "simpleCondition", ...)
+  } else {
+    code <- .cstr_apply(list(x$message, call = x$call), "simpleCondition", ...)
+  }
+  repair_attributes_simpleCondition(x_bkp, code, ...)
+}
+
+repair_attributes_simpleCondition <- function(x, code, pipe = NULL, ...) {
+  .cstr_repair_attributes(
+    x, code, pipe,
+    idiomatic_class = c("simpleCondition", "condition"),
+    ...
+  )
+}
---
+++ tests/testthat/_snaps/s3-simpleCondition.md
@@ -0,0 +1,11 @@
+# simpleCondition
+
+    Code
+      construct(simpleCondition("hello"))
+    Output
+      simpleCondition("hello")
+    Code
+      construct(simpleCondition("hello", call = quote(a())))
+    Output
+      simpleCondition("hello", call = quote(a()))
+
