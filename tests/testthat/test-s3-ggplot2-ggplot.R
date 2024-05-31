e54a8c4ad23accf9baaf9176316da6bb5aaaed08
Antoine Fabri <antoine.fabri@gmail.com>
2024-05-31 11:01:11 +0200

    new test and snapshot

--- tests/testthat/test-s3-ggplot2-ggplot.R
+++ tests/testthat/test-s3-ggplot2-ggplot.R
@@ -4,6 +4,10 @@ test_that("ggplot", {
     p1 <- base_99 + ggplot2::scale_x_continuous(limits = c(1, 7))
     p2 <- p1 + ggplot2::scale_y_continuous(limits = c(10, 45)) + ggplot2::facet_wrap(~manufacturer)
 
+    expect_pipe_snapshot({
+      construct(p1, data = list(mpg_99 = mpg_99))
+      construct(p2, data = list(mpg_99 = mpg_99))
+    })
     expect_faithful_ggplot_construction(base_99, opts_Layer("layer"))
     expect_faithful_ggplot_construction(base_99, opts_Layer("environment"))
     expect_faithful_ggplot_construction(p1, opts_Layer("environment"))
