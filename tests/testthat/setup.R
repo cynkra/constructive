ce74631085fb8770b513198a6a8f92a65b06cae2
Antoine Fabri <antoine.fabri@gmail.com>
2024-06-29 21:41:17 +0200

    improve override so it handles code containing {{ }}

--- tests/testthat/setup.R
+++ tests/testthat/setup.R
@@ -17,10 +17,14 @@ colon_colon <- `::`
 length <- base::length
 
 expect_snapshot <- function(code) {
-  transform <- function(out) {
-    out <- gsub("%>%", "|>", out, fixed = TRUE)
-    out <- gsub("= [.]([,)])", "= _\\1", out)
-    out
-  }
-  rlang::inject(testthat::expect_snapshot({{ code }}, transform = transform))
+  eval.parent(substitute(
+    testthat::expect_snapshot(
+      code,
+      transform = function(out) {
+        out <- gsub("%>%", "|>", out, fixed = TRUE)
+        out <- gsub("= [.]([,)])", "= _\\1", out)
+        out
+      }
+    )
+  ))
 }
