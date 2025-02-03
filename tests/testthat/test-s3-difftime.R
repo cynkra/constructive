6546f9e825a9943f2f7a787e67b88d88027a9137
Antoine Fabri <antoine.fabri@gmail.com>
2025-02-03 15:21:46 +0100

    add test

--- tests/testthat/test-s3-difftime.R
+++ tests/testthat/test-s3-difftime.R
@@ -2,6 +2,12 @@ test_that("difftime", {
   expect_snapshot({
     construct(as.difftime(1, units = "secs"))
     construct(as.difftime(2, units = "mins"))
+    x <- structure(
+      c(NA, 74269, 39024, 64597, 24937, NA, NA, 50690, 19113),
+      class = c("foo", "difftime"),
+      units = "secs"
+    )
+    construct(x)
   })
 })
 
