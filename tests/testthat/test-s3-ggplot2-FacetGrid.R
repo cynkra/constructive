d1d8095e5cddd5a09f6af1226a86755f7fd65780
Antoine Fabri <antoine.fabri@gmail.com>
2025-04-24 20:52:17 +0200

    Implement FacetGrid

---
+++ tests/testthat/test-s3-ggplot2-FacetGrid.R
@@ -0,0 +1,19 @@
+test_that("FacetGrid", {
+  mpg <- ggplot2::mpg
+  p <- ggplot2::ggplot(mpg, ggplot2::aes(displ, cty)) + ggplot2::geom_point()
+  p2 <- p + ggplot2::facet_grid(rows = ggplot2::vars(drv))
+
+  p3 <- ggplot2::ggplot(mpg, ggplot2::aes(cty, hwy)) +
+    ggplot2::geom_point() +
+    ggplot2::facet_grid(year ~ drv, axes = "all", axis.labels = "all_x")
+
+  p4 <- ggplot2::ggplot(mpg, ggplot2::aes(drv, model)) +
+    ggplot2::geom_point() +
+    ggplot2::facet_grid(manufacturer ~ ., scales = "free", space = "free") +
+    ggplot2::theme(strip.text.y = ggplot2::element_text(angle = 0))
+  expect_snapshot({
+    construct(p2, data = list(mpg = mpg))
+    construct(p3, data = list(mpg = mpg))
+    construct(p4, data = list(mpg = mpg), check = FALSE)
+  })
+})
