0098891779f1f2d04547ebd56c939587284b100a
Antoine Fabri <antoine.fabri@gmail.com>
2025-08-02 14:28:20 +0200

    fix expect_construct()

--- R/expect_construct.R
+++ R/expect_construct.R
@@ -1,16 +1,12 @@
-expect_construct <- function(x, expected) {
-  # for convenience when calling expect_construct(x,) with the comma we print
-  # the expr
-  if (missing(expected) && length(sys.call()) > 2) {
-    construct_call <- substitute(construct(x))
-    out <- construct(x, check = FALSE)$code
+expect_construct <- function(x, expected, ...) {
+  if (!missing(expected) && rlang::is_na(expected)) {
+    out <- construct(x, check = FALSE, ...)$code
     getFromNamespace("write_clip", "clipr")(out)
     return(out)
   }
 
   expected <- if (missing(expected)) substitute(x) else substitute(expected)
-  construct_call <- substitute(construct(x))
-  new_code <- construct(x, check = FALSE)$code
+  new_code <- construct(x, check = FALSE, ...)$code
   recreated <- parse(text=new_code)[[1]]
   expect_equal(recreated, expected)
 }
