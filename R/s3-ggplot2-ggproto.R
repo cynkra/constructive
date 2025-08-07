2ae85133bdcba41bcd9621047de06b877368c2b6
Antoine Fabri <antoine.fabri@gmail.com>
2025-08-07 11:10:25 +0200

    improve old ggproto construction

--- R/s3-ggplot2-ggproto.R
+++ R/s3-ggplot2-ggproto.R
@@ -26,18 +26,6 @@ is_corrupted_ggproto <- function(x) {
 #' @export
 #' @method .cstr_construct.ggproto default
 .cstr_construct.ggproto.default <- function(x, ..., ggproto.ignore_draw_key = FALSE) {
-  # archive old code
-  if (with_versions(ggplot2 <= "3.5.2")) {
-    if (ggproto.ignore_draw_key) {
-      x <- as.list(x)
-      x$draw_key <- NULL
-    }
-    code <- find_in_package_protos(x, ggproto.ignore_draw_key)
-    if (is.null(code)) return(.cstr_construct.environment(x, ...))
-    code <- repair_attributes_ggproto(x, code, ...)
-    return(code)
-  }
-
   pkg_protos <- Filter(function(x) inherits(x, "ggproto"), as.list(asNamespace("ggplot2")))
   ind <- sapply(pkg_protos, identical, x)
   if (any(ind)) {
@@ -77,26 +65,4 @@ is_corrupted_ggproto <- function(x) {
 
 repair_attributes_ggproto <- function(x, code, pipe = NULL, ...) {
   code
-}
-
-find_in_package_protos <- function(x, ggproto.ignore_draw_key) {
-  # only for ggplot3 before and including 3.5.2
-  # this seems problematic but hasn't been rechecked with old code
-  for (pkg in globals$ggpackages) {
-    pkg_protos <- Filter(function(x) inherits(x, "ggproto"), as.list(asNamespace(pkg)))
-    for (nm in names(pkg_protos)) {
-      proto <- pkg_protos[[nm]]
-      if (ggproto.ignore_draw_key) {
-        proto <- as.list(proto)
-        proto$draw_key <- NULL
-      }
-      if (identical(x, proto, ignore.environment = TRUE)) {
-        if (pkg == "ggplot2") {
-          # FIXME: not good enough GeomContourFixed -> countour_fixed ?
-          return(sprintf('"%s"', sub("^[^_]+_", "", snakeize(nm))))
-        }
-        return(paste0(pkg, ":::", nm))
-      }
-    }
-  }
 }
