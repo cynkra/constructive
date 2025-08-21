ef75eb3399dc8c9c6a7e7483178ddc5177a518ff
Antoine Fabri <antoine.fabri@gmail.com>
2025-08-21 19:33:17 +0200

    skip S7_object test on linux

--- tests/testthat/test-s7-S7-object.R
+++ tests/testthat/test-s7-S7-object.R
@@ -1,4 +1,6 @@
 test_that("S7_object", {
+  # FIXME see comment in S/_class tests
+  testthat::skip_on_os("linux")
   expect_snapshot({
     construct(S7::S7_object())
     construct(S7::S7_object(), opts_S7_object("next"))
