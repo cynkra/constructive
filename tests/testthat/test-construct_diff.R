9789f490bec9ca6e915cfdf241755bc88981db7c
Antoine Fabri <antoine.fabri@gmail.com>
2025-08-22 00:52:34 +0200

    attempt to fix skip

--- tests/testthat/test-construct_diff.R
+++ tests/testthat/test-construct_diff.R
@@ -2,7 +2,7 @@ test_that("construct_diff", {
   # stangely we have a different indention for ubuntu with R 4.1 (one more space)
   # it's hard to solve and doesn't seem that important so we just skip the test
   # for this situation
-  if (with_versions(R == "4.1.0")) skip_on_os("linux")
+  if (with_versions(R < "4.2.0")) skip_on_os("linux")
 
   expect_snapshot({
     construct_diff(
