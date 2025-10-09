428c132f998bfd12e7ec84efa1bc468fea677b89
Antoine Fabri <antoine.fabri@gmail.com>
2025-10-09 16:16:11 +0200

    S4 construction falls back to .cstr_construct.object.prototype for old R when no S4 bit

--- R/s4.R
+++ R/s4.R
@@ -21,6 +21,13 @@ opts_S4 <- function(constructor = c("new", "prototype"), ...) {
 .cstr_construct.S4 <- function(x, ...) {
   opts <- list(...)$opts$S4 %||% opts_S4()
   if (is_corrupted_S4(x) || opts$constructor == "next") return(NextMethod())
+  if (with_versions(R < "4.4") && !isS4(x)) {
+    # in R < 4.4 we can have objects of type "S4" without the S4 bit on
+    # The S4 bit is recognized by `isS4()` or `isS3()` and set or unset by
+    # `asS4()` or `asS3()`, for later R there is a new type "object" for those
+    # and we can reuse their construction
+    return(.cstr_construct.object.prototype(x, ...))
+  }
   UseMethod(".cstr_construct.S4", structure(NA, class = opts$constructor))
 }
 
